fbc4829830299c7ba5f9287660175bd4ce297c4d
gyubo <yoon9yubo@gmail.com>
2025-01-22 21:16:48 +0900

    state fetching network thortthing

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,22 +3,27 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlacese] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlacese(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlacese(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="장소를 가져오고 있어요..!"
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
