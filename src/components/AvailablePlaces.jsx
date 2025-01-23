f22e4b7fac751a1a6fe90c2177cb3b3994931554
gyubo <yoon9yubo@gmail.com>
2025-01-24 01:41:11 +0900

    커스텀훅, 사용자위치 확인, navigator

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,43 +1,55 @@
-import { useState, useEffect } from "react";
-
 import Places from "./Places.jsx";
 import Error from "./Modal.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../hooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+
+      // setAvailablePlacese(sortedPlaces);
+      // setIsFetching(false);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlacese] = useState([]);
-  const [error, setError] = useState();
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+    // setFetchedData: setAvailablePlacese,
+  } = useFetch(fetchSortedPlaces, []);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
+  // useEffect(() => {
+  //   async function fetchPlaces() {
+  //     setIsFetching(true);
 
-      try {
-        const places = await fetchAvailablePlaces();
+  //     try {
+  //       const places = await fetchAvailablePlaces();
 
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlacese(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message ||
-            "장소를 불러오지 못했습니다. 나중에 다시 시도해주세요.",
-        });
-        setIsFetching(false);
-      }
-    }
+  //     } catch (error) {
+  //       setError({
+  //         message:
+  //           error.message ||
+  //           "장소를 불러오지 못했습니다. 나중에 다시 시도해주세요.",
+  //       });
+  //       setIsFetching(false);
+  //     }
+  //   }
 
-    fetchPlaces();
-  }, []);
+  //   fetchPlaces();
+  // }, []);
 
   if (error) {
     return <Error title="오류가 발생했습니다." message={error.message} />;
