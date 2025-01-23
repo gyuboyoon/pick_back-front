225c424cb75ba4ccdff9de144c51e059269b3144
gyubo <yoon9yubo@gmail.com>
2025-01-23 21:18:19 +0900

    navigator.geolocation , 사용자 위치 가져오기 콜백함수 지정

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,22 +1,51 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Modal.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlacese] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlacese(resData.places);
-      setIsFetching(false);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlacese(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message ||
+            "장소를 불러오지 못했습니다. 나중에 다시 시도해주세요.",
+        });
+        setIsFetching(false);
+      }
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="오류가 발생했습니다." message={error.message} />;
+  }
 
   return (
     <Places
